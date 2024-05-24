7d6d271790ba2cc72fdf619587e0451a96303200
Philopateer Nabil <philopateernabil2021@gmail.com>
2024-05-24 16:15:15 +0300

    fix data generator injection.

--- services/mock_data_generator/service.go
+++ services/mock_data_generator/service.go
@@ -3,28 +3,19 @@ package mockDataGenerator
 import (
 	"Bete/services/database"
 	"go.uber.org/fx"
-	"gorm.io/gorm"
 )
-
-type mockDataGenerator struct {
-	db *gorm.DB
-}
 
 type params struct {
 	fx.In
 	DbService database.Service
 }
 
-func New(p params) Service {
-	return &mockDataGenerator{
-		db: p.DbService.GetDBInstance(),
-	}
-}
+func GenerateData(p params) {
+	db := p.DbService.GetDBInstance()
 
-func (s mockDataGenerator) GenerateData() {
-	s.db.FirstOrCreate(&restaurant, restaurant)
+	db.FirstOrCreate(&restaurant, restaurant)
 	branch.RestaurantID = restaurant.ID
-	s.db.FirstOrCreate(&branch, branch)
+	db.FirstOrCreate(&branch, branch)
 	menu.RestaurantID = restaurant.ID
-	s.db.FirstOrCreate(&menu, menu)
+	db.FirstOrCreate(&menu, menu)
 }
